919fe8b584304f82e777df25a59d171cfc4d06b6
Jacob Alford <jacob.alford@me.com>
2019-08-03 17:14:15 +0000

    Added view post button to blogbar

--- src/components/sentences/BlogBarActions/index.js
+++ src/components/sentences/BlogBarActions/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
 import { IconButton, Fade } from '@material-ui/core/';
-import { NoteAdd , Edit } from '@material-ui/icons/';
+import { NoteAdd , Edit , Pageview } from '@material-ui/icons/';
 
 export default function BlogBarActions(props){
   const { user , context , color , setCreatePostIsOpen , history , match } = props;
   const openCreatePost = () => setCreatePostIsOpen(true);
   const editPost = () => history.push(`/blog/edit/${match.params.postId}`);
+  const viewPost = () => history.push(`/blog/view/${match.params.postId}`);
   return (
     <React.Fragment>
       {(user.activeUser.permissions.value >= 8 && ["inPostView","inBlog"].includes(context)) ? (
@@ -20,6 +21,13 @@ export default function BlogBarActions(props){
         <Fade in={user.loggedIn} timeout={1500}>
           <IconButton onClick={editPost} color="inherit">
             <Edit style={{color:color}}/>
+          </IconButton>
+        </Fade>
+        ) : null}
+      {(user.activeUser.permissions.value >= 8 && context==="inPostEdit") ? (
+        <Fade in={user.loggedIn} timeout={1500}>
+          <IconButton onClick={viewPost} color="inherit">
+            <Pageview style={{color:color}}/>
           </IconButton>
         </Fade>
         ) : null}
