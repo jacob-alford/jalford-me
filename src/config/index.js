e82e8e2d054f4e2e65554febc04ed64c468e4255
Jacob Alford - MBP <jacob.alford@me.com>
2020-01-18 11:50:11 -0700

    UPGRADE + (prettier)
    Upgraded packages 4 fun, and added prettier

--- src/config/index.js
+++ src/config/index.js
@@ -2,26 +2,27 @@ import linkedIn from 'assets/social/LinkedIn.png';
 import icecaveImage from 'assets/photos/IceCaves_onHome.webp';
 
 export const socialMedia = {
-  linkedIn:{
-    img:linkedIn,
-    url:"https://www.linkedin.com/in/jacob-alford/"
-  },
-  email:"jalford-website@pm.me"
-}
+	linkedIn: {
+		img: linkedIn,
+		url: 'https://www.linkedin.com/in/jacob-alford/'
+	},
+	email: 'jalford-website@pm.me'
+};
 
 export const navItems = [
-  {text:"Home",url:"/"},
-  {text:"About/Contact",url:"/about"},
-  {text:"Media",url:"/media"},
-  {text:"Websites",url:"/websites"},
-  {text:"Posts",url:"/posts"},
-  {text:"Resume",url:"https://www.visualcv.com/jacob-alford"}
+	{ text: 'Home', url: '/' },
+	{ text: 'About/Contact', url: '/about' },
+	{ text: 'Puzzles', url: '/puzzles' },
+	{ text: 'Websites', url: '/websites' },
+	{ text: 'Posts', url: '/posts' },
+	{ text: 'Resume', url: 'https://www.visualcv.com/jacob-alford' }
 ];
 
 export const homePageImage = {
-  img:icecaveImage,
-  caption:"The Ice Cave",
-  body:"A breathtaking diversion in Grants, NM.  It's where I grew up! My family has owned it for generations."
-}
+	img: icecaveImage,
+	caption: 'The Ice Cave',
+	body:
+		"A breathtaking diversion in Grants, NM.  It's where I grew up! My family has owned it for generations."
+};
 
-export const footerText = "Copyright © 2019 Jacob Alford";
+export const footerText = 'Copyright © 2019 Jacob Alford';
