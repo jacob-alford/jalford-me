758af20d9072f7a4a88dac2489aa29cd3e86418e
Jacob Alford <jacob.alford@me.com>
2020-01-18 22:30:55 +0000

    MediaUpload Changes?
    I don't really know, I'm on a different computer, and it's been a while

--- src/components/novels/MediaUpload/index.js
+++ src/components/novels/MediaUpload/index.js
@@ -1,4 +1,4 @@
-import React , { useState } from 'react';
+import React , { useState , useRef } from 'react';
 
 import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
@@ -7,9 +7,13 @@ import InputBase from '@material-ui/core/InputBase';
 import LightDarkToggler from 'components/words/LightDarkToggler';
 import Holder from 'components/words/Holder';
 
+import Category from '@material-ui/icons/Category';
+
 import useTLD from 'components/bindings/hooks/useTLD';
 import useTitleSize from 'components/bindings/hooks/useTitleSize';
 import { themeHook } from 'theme';
+
+const getBorderColor = tldState => (tldState === 'light') ? "rgba(0,0,0,.87)" : 'rgba(255,255,255,1)';
 
 const useClasses = themeHook(
   [
@@ -29,7 +33,8 @@ const useClasses = themeHook(
       paddingBottom:majorSpacing
     },
     title:{
-      color:({tldState}) => (tldState === 'light') ? "rgba(0,0,0,.87)" : 'rgba(255,255,255,1)'
+      color:({tldState}) => (tldState === 'light') ? "rgba(0,0,0,.87)" : 'rgba(255,255,255,1)',
+      transition: 'color .5s'
     },
     togglerHolder:{
       width:'100%',
@@ -37,11 +42,25 @@ const useClasses = themeHook(
     },
     textBox:{
       color:({tldState}) => (tldState === 'light') ? "rgba(0,0,0,.87)" : 'rgba(255,255,255,1)',
-      transition: 'background .5s, color .5s',
-      backgroundColor:({tldState}) => (tldState !== 'light') ? "rgba(0,0,0,.95)" : 'rgba(255,255,255,.95)',
-      borderRadius,
+      borderBottom:({tldState}) => `solid 1px ${getBorderColor(tldState)}`,
+      transition: 'background .5s, color .5s, border .5s'
+    },
+    textBoxHolder:{
+      marginTop:majorSpacing,
+      border:({tldState}) => `solid 1px ${getBorderColor(tldState)}`,
       padding:minorSpacing,
-      borderColor:({tldState}) => (tldState === 'light') ? "rgba(0,0,0,.87)" : 'rgba(255,255,255,1)',
+      width:'222px',
+      position:'relative',
+      left:'calc(50% - 130px)',
+      borderRadius,
+      transition: 'border .5s'
+    },
+    categoryIcon:{
+      color:({tldState}) => (tldState === 'light') ? "rgba(0,0,0,.87)" : 'rgba(255,255,255,1)',
+      transition: 'color .5s',
+      marginRight:'15px'
+    },
+    fileUpload:{
       marginTop:majorSpacing
     }
   })
@@ -52,7 +71,11 @@ export default function MediaUpload(){
   const classes = useClasses({tldState});
   const {h2:fontSize} = useTitleSize();
   const [category,setCategory] = useState("");
+  const formRef = useRef();
   const handleType = evt => setCategory(evt.target.value);
+  const handleUpload = () => {
+
+  }
   return (
     <Container className={classes.photoUploadHolder}>
       <Holder className={classes.togglerHolder} justify='flex-end' direction='row'>
@@ -61,12 +84,19 @@ export default function MediaUpload(){
       <Typography variant="h2" style={{fontSize}} className={classes.title}>
         Image Upload
       </Typography>
-      <InputBase
-        variant="outlined"
-        label="Category"
-        className={classes.textBox}
-        value={category}
-        onChange={handleType}/>
+      <Holder direction="row" className={classes.textBoxHolder}>
+        <Category className={classes.categoryIcon}/>
+        <InputBase
+          variant="outlined"
+          label="Category"
+          placeholder="Category"
+          className={classes.textBox}
+          value={category}
+          onChange={handleType}/>
+      </Holder>
+      <Holder className={classes.fileUpload}>
+        <input ref={formRef} type='file' accept="image/png, image/jpeg" name="photo" multiple/>
+      </Holder>
     </Container>
   );
 }
