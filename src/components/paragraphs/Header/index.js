b1429350da152cb8f6aa4b536abec6075120179a
Jacob Alford - MBP <jacob.alford@me.com>
2020-01-18 15:26:48 -0700

    Blog Listing Style-Uniformize
    Resized the blog to match the ux grid, and had to hide the snippit if the screen was too small.  Also, (((prettier)))

--- src/components/paragraphs/Header/index.js
+++ src/components/paragraphs/Header/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useSpring , animated as a } from 'react-spring';
+import { useSpring, animated as a } from 'react-spring';
 
 import Typography from '@material-ui/core/Typography';
 
@@ -10,56 +10,55 @@ import { navItems } from 'config';
 import { themeHook } from 'theme';
 
 const useClasses = themeHook({
-  title:{
-    fontWeight:'bold',
-    fontSize:'3.5rem',
-    textAlign:'center',
-    color:'black'
-  },
-  image:{
-    width:'350px'
-  },
-  holder:{
-    display:'flex',
-    justifyContent:'center',
-    alignItems:'center',
-    flexDirection:'column'
-  },
-  colorBlue:{
-    color:'#69beef'
-  },
-  colorBlueGray:{
-    color:'#212832'
-  },
-  colorGray:{
-    color:'#262626'
-  }
+	title: {
+		fontWeight: 'bold',
+		fontSize: '3.5rem',
+		textAlign: 'center',
+		color: 'black'
+	},
+	image: {
+		width: '350px'
+	},
+	holder: {
+		display: 'flex',
+		justifyContent: 'center',
+		alignItems: 'center',
+		flexDirection: 'column'
+	},
+	colorBlue: {
+		color: '#69beef'
+	},
+	colorBlueGray: {
+		color: '#212832'
+	},
+	colorGray: {
+		color: '#262626'
+	}
 });
 
-
 const getScaleString = headerIsOpen => {
-  const scale = (headerIsOpen) ? 1 : 0;
-  return `scale3d(${scale},${scale},${scale})`;
-}
+	const scale = headerIsOpen ? 1 : 0;
+	return `scale3d(${scale},${scale},${scale})`;
+};
 
-export default function Header(props){
-  const { headerIsOpen } = props;
-  const classes = useClasses();
-  const interStyles = useSpring({
-    opacity:(headerIsOpen) ? 1 : 0,
-    transform:getScaleString(headerIsOpen),
-    from:{
-      transform:getScaleString(false)
-    }
-  });
-  return (
-    <a.div style={interStyles} className={classes.holder}>
-      <Typography variant="h1" className={classes.title} paragraph>
-        <span className={classes.colorBlueGray}>j</span>
-        <span className={classes.colorBlue}>a</span>
-        <span className={classes.colorGray}>lford</span>
-      </Typography>
-      <NavBar navList={navItems} />
-    </a.div>
-  );
+export default function Header(props) {
+	const { headerIsOpen } = props;
+	const classes = useClasses();
+	const interStyles = useSpring({
+		opacity: headerIsOpen ? 1 : 0,
+		transform: getScaleString(headerIsOpen),
+		from: {
+			transform: getScaleString(false)
+		}
+	});
+	return (
+		<a.div style={interStyles} className={classes.holder}>
+			<Typography variant='h1' className={classes.title} paragraph>
+				<span className={classes.colorBlueGray}>j</span>
+				<span className={classes.colorBlue}>a</span>
+				<span className={classes.colorGray}>lford</span>
+			</Typography>
+			<NavBar navList={navItems} />
+		</a.div>
+	);
 }
