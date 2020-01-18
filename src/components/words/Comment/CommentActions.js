54d0dfe134c8bbea8f0150909a05000df7599c68
Jacob Alford <jacob.alford@me.com>
2020-01-18 23:20:18 +0000

    More Dark Mode
    Finished dark mode for comments (((prettier))]

--- src/components/words/Comment/CommentActions.js
+++ src/components/words/Comment/CommentActions.js
@@ -1,64 +1,78 @@
-import React , { useState } from 'react';
+import React, { useState } from 'react';
 
 import IconButton from '@material-ui/core/IconButton';
 import EditIcon from '@material-ui/icons/Edit';
 import DeleteIcon from '@material-ui/icons/Delete';
 import DeleteForeverIcon from '@material-ui/icons/DeleteForever';
 
-export default function CommentActions(props){
-  const [confirmDelete,setConfirmDelete] = useState(false);
-  const [confirmPermDelete,setConfirmPermDelete] = useState(false);
-  const {
-    activeUser,
-    commentUser,
-    edit,
-    remove,
-    permDelete,
-    isEditing
-  } = props;
-  return (
-    <React.Fragment>
-      {(activeUser.uid === commentUser.uid) ? (
-        <IconButton onClick={edit} disabled={isEditing}>
-          <EditIcon />
-        </IconButton>
-      ) : null}
-      {(activeUser.uid === commentUser.uid || activeUser.permissions.value === 10) ? (
-        <IconButton
-          onClick={
-            (confirmDelete) ?
-              () => {
-                setConfirmDelete(false);
-                remove();
-              }
-            : () => setConfirmDelete(true)
-          }
-          color={
-            (confirmDelete) ?
-              "secondary"
-            : "primary"
-          }>
-          <DeleteIcon />
-        </IconButton>
-      ) : null}
-      {(activeUser.permissions.value === 10) ? (
-        <IconButton
-          onClick={
-            (confirmPermDelete) ?
-              () => {
-                setConfirmPermDelete(false);
-                permDelete();
-              }
-            : () => setConfirmPermDelete(true)
-          }
-          color={
-            (confirmPermDelete) ?
-              "secondary"
-            : "primary"
-          }>
-          <DeleteForeverIcon />
-        </IconButton>
-      ) : null}
-    </React.Fragment>
-  );
+import useTLD from 'components/bindings/hooks/useTLD';
+
+import { themeHook } from 'theme';
+
+const getLightIconColor = isEditing =>
+	isEditing ? 'rgba(0,0,0,.5)' : 'rgba(0,0,0,1)';
+const getDarkIconColor = isEditing =>
+	isEditing ? 'rgba(255,255,255,.5)' : 'rgba(255,255,255,1)';
+const getIconColor = (tldState, isEditing) =>
+	tldState === 'light'
+		? getLightIconColor(isEditing)
+		: getDarkIconColor(isEditing);
+
+const useClasses = themeHook({
+	edit: {
+		color: ({ tldState, isEditing }) => getIconColor(tldState, isEditing)
+	}
+});
+
+export default function CommentActions(props) {
+	const [confirmDelete, setConfirmDelete] = useState(false);
+	const [confirmPermDelete, setConfirmPermDelete] = useState(false);
+	const [tldState] = useTLD();
+	const classes = useClasses({ tldState, ...props });
+	const {
+		activeUser,
+		commentUser,
+		edit,
+		remove,
+		permDelete,
+		isEditing
+	} = props;
+	return (
+		<React.Fragment>
+			{activeUser.uid === commentUser.uid ? (
+				<IconButton onClick={edit} disabled={isEditing}>
+					<EditIcon className={classes.edit} />
+				</IconButton>
+			) : null}
+			{activeUser.uid === commentUser.uid ||
+			activeUser.permissions.value === 10 ? (
+				<IconButton
+					onClick={
+						confirmDelete
+							? () => {
+									setConfirmDelete(false);
+									remove();
+							  }
+							: () => setConfirmDelete(true)
+					}
+					color={confirmDelete ? 'secondary' : 'primary'}>
+					<DeleteIcon />
+				</IconButton>
+			) : null}
+			{activeUser.permissions.value === 10 ? (
+				<IconButton
+					onClick={
+						confirmPermDelete
+							? () => {
+									setConfirmPermDelete(false);
+									permDelete();
+							  }
+							: () => setConfirmPermDelete(true)
+					}
+					color={confirmPermDelete ? 'secondary' : 'primary'}>
+					<DeleteForeverIcon />
+				</IconButton>
+			) : null}
+		</React.Fragment>
+	);
 }
--- src/components/words/NewComment/index.js
+++ src/components/words/NewComment/index.js
@@ -1,4 +1,4 @@
-import React , { useState } from 'react';
+import React, { useState } from 'react';
 
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
@@ -11,62 +11,71 @@ import Holder from 'components/words/Holder';
 import Loader from 'components/words/Loader';
 
 import useRHook from 'components/bindings/hooks/useRHook';
+import useTLD from 'components/bindings/hooks/useTLD';
 
 import { themeHook } from 'theme';
 
 const useClasses = themeHook({
-  loginMessage:{
-    color:'rgba(0,0,0,.64)',
-    marginTop:'8px',
-    textAlign:'center'
-  }
+	loginMessage: {
+		color: ({ tldState }) =>
+			tldState === 'light' ? 'rgba(0,0,0,.87)' : 'rgba(255,255,255,1)',
+		marginTop: '8px',
+		textAlign: 'center'
+	},
+	newComment: {
+		background: ({ tldState }) => (tldState === 'light' ? '#fff' : '#454545')
+	},
+	commentText: {
+		color: ({ tldState }) =>
+			tldState === 'light' ? 'rgba(0,0,0,.87)' : 'rgba(255,255,255,1)'
+	}
 });
-
-export default function NewComment(props){
-  const { addComment , closeModal = val => val } = props;
-  const { user , userLoading } = useRHook();
-  const [commentText,setCommentText] = useState("");
-  const classes = useClasses();
-  const handleCommentEdit = evt => setCommentText(evt.target.value);
-  if(userLoading) return <Loader />;
-  return (
-    <Card>
-      <CardContent>
-        <Holder>
-         {(!user.loggedIn) ? (
-            <Typography variant="body1" className={classes.loginMessage}>
-              log in to post a comment
-            </Typography>
-          ) : (
-            <React.Fragment>
-              <TextField
-                multiline
-                fullWidth
-                label="New Comment"
-                onChange={handleCommentEdit}
-                value={commentText} />
-              <Typography variant="body1" className={classes.loginMessage}>
-                **<strong>[bold]</strong>** —
-                *<i>[italic]</i>* —
-                ~~<strike>[strikethrough]</strike>~~
-              </Typography>
-            </React.Fragment>
-          )}
-        </Holder>
-      </CardContent>
-      {(user.loggedIn) ? (
-        <CardActions>
-          <Button
-            color='primary'
-            disabled={!user.loggedIn || commentText === ""}
-            onClick={() => {
-              addComment(commentText);
-              closeModal();
-            }}>
-            Submit
-          </Button>
-        </CardActions>
-      ) : null}
-    </Card>
-  );
+export default function NewComment(props) {
+	const { addComment, closeModal = val => val } = props;
+	const { user, userLoading } = useRHook();
+	const [tldState] = useTLD();
+	const [commentText, setCommentText] = useState('');
+	const classes = useClasses({ tldState });
+	const handleCommentEdit = evt => setCommentText(evt.target.value);
+	if (userLoading) return <Loader />;
+	return (
+		<Card>
+			<CardContent>
+				<Holder>
+					{!user.loggedIn ? (
+						<Typography variant='body1' className={classes.loginMessage}>
+							log in to post a comment
+						</Typography>
+					) : (
+						<React.Fragment>
+							<TextField
+								multiline
+								fullWidth
+								label='New Comment'
+								onChange={handleCommentEdit}
+								value={commentText}
+							/>
+							<Typography variant='body1' className={classes.loginMessage}>
+								**<strong>[bold]</strong>** — *<i>[italic]</i>* — ~~
+								<strike>[strikethrough]</strike>~~
+							</Typography>
+						</React.Fragment>
+					)}
+				</Holder>
+			</CardContent>
+			{user.loggedIn ? (
+				<CardActions>
+					<Button
+						color='primary'
+						disabled={!user.loggedIn || commentText === ''}
+						onClick={() => {
+							addComment(commentText);
+							closeModal();
+						}}>
+						Submit
+					</Button>
+				</CardActions>
+			) : null}
+		</Card>
+	);
 }
