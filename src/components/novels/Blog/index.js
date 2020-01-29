964e7e95603df391ca20220cb75b2a7f1a2f5f60
Jacob Alford <jacob.alford@me.com>
2020-01-29 03:31:15 +0000

    Remove Philosophy from Blog pending restructure

--- src/components/novels/Blog/index.js
+++ src/components/novels/Blog/index.js
@@ -1,18 +1,14 @@
 import React from 'react';
 
 import DuncanStrauss from 'components/sentences/BlogSeries/DuncanStrauss';
-import Philosophy from 'components/sentences/BlogSeries/Philosophy';
-
 import withPageFade from 'components/bindings/wrappers/withPageFade';
 
 function Blog() {
-  return (
-    <React.Fragment>
-      <DuncanStrauss minHeight={1000} widthStr="100vw"/>
-      <Philosophy minHeight={1000} widthStr="100vw"/>
-    </React.Fragment>
-
-  );
+	return (
+		<React.Fragment>
+			<DuncanStrauss minHeight={1000} widthStr='100vw' />
+		</React.Fragment>
+	);
 }
 
 export default withPageFade(Blog);
